92e99fee7d500af875614f018756a959772c8230
mpgxc <mpgx5.c@gmail.com>
2020-10-16 10:56:30 -0300

    chore: call the user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import orphanageRouter from './orphanage.routes';
 import sessionRouter from './session.routes';
+import userRouter from './user.routes';
 
 const routes = Router();
 
 routes.use('/orphanage', orphanageRouter);
 routes.use('/session', sessionRouter);
+routes.use('/user', userRouter);
 
 export default routes;
